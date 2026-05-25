56084a758a244819721d6cfce16427fdc3f97dbb
Anubha Parashar <anubhaparashar1025@gmail.com>
2026-05-26 01:23:59 +0530

    1

--- js/site-components.js
+++ js/site-components.js
@@ -47,6 +47,7 @@
     '                aria-haspopup="true" aria-expanded="false">Projects</a>' +
     '              <ul class="dropdown-menu">' +
     '                <li class="nav-item"><a class="nav-link" href="project.html">Projects</a></li>' +
+    '                <li class="nav-item"><a class="nav-link" href="https://gaitai-analysis.github.io/GaitAI/" target="_blank" rel="noopener">GaitAI</a></li>' +
     '                <li class="nav-item"><a class="nav-link" href="grant.html">Grant</a></li>' +
     '                <li class="nav-item"><a class="nav-link" href="industry.html">Industry</a></li>' +
     '              </ul>' +
