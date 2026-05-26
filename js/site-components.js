9b8ce4ef7c2197373e2831484815645e335f82ec
Anubha Parashar <anubhaparashar1025@gmail.com>
2026-05-26 08:40:10 +0530

    Use clearer header name logo

--- js/site-components.js
+++ js/site-components.js
@@ -24,7 +24,7 @@
     '    <nav class="navbar navbar-expand-lg navbar-light">' +
     '      <div class="container">' +
     '        <a class="navbar-brand logo_h" href="index.html">' +
-    '          <img class="nav-logo-img" src="files/1.%20Home/logo.png" alt="Dr. Anubha Parashar">' +
+    '          <img class="nav-logo-img" src="files/1.%20Home/logo-header-name-only.png" alt="Dr. Anubha Parashar">' +
     '        </a>' +
     '        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"' +
     '          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
