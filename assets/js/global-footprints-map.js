da60e2ac3e4e8e70e5d09fab9dce541abde76104
Anubha Parashar <anubhaparashar1025@gmail.com>
2026-06-29 06:03:48 +0530

    11

--- assets/js/global-footprints-map.js
+++ assets/js/global-footprints-map.js
@@ -257,7 +257,7 @@
       attributionControl: true
     }).setView([24, 32], 2);
     window.L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
       subdomains: 'abcd',
       maxZoom: 19
     }).addTo(map);
