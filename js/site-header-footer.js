9c61b91013478ec3d47eb8de87ef9215192fa1c2
Anubha Parashar <anubhaparashar1025@gmail.com>
2026-07-16 09:12:47 +0530

    Cache bust shared site components

--- js/site-header-footer.js
+++ js/site-header-footer.js
@@ -1,5 +1,5 @@
 (function () {
   if (window.__siteComponentsRequested) return;
   window.__siteComponentsRequested = true;
-  document.write('<script src="js/site-components.js"><\/script>');
+  document.write('<script src="js/site-components.js?v=f8fbb99"><\/script>');
 })();
