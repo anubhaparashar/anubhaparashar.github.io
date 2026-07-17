2bb2ac95b34e738a859e3f1b72fd45cb8ed8ab94
Anubha Parashar <anubhaparashar1025@gmail.com>
2026-07-17 14:13:33 +0530

    Compact experience typography and restore footer icon

--- js/site-header-footer.js
+++ js/site-header-footer.js
@@ -1,5 +1,5 @@
 (function () {
   if (window.__siteComponentsRequested) return;
   window.__siteComponentsRequested = true;
-  document.write('<script src="js/site-components.js?v=counter-audit-20260717"><\/script>');
+  document.write('<script src="js/site-components.js?v=hf-icon-20260717"><\/script>');
 })();
